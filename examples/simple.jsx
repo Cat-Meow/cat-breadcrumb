aebd6650600212c26c65cc622c79761d5cb49527
yanweiqing <yanweiqing@youzan.com>
2016-02-29 20:52:39 +0800

    breadcrumb init

---
+++ examples/simple.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import BreadCrumb from '../src/breadcrumb.js';
+import '../assets/index.less';
+
+export default class Example extends Component {
+    render() {
+        return (
+            <BreadCrumb>
+                <BreadCrumb.Crumb href="#">Home</BreadCrumb.Crumb>
+                <BreadCrumb.Crumb href="">Link</BreadCrumb.Crumb>
+                <BreadCrumb.Crumb>Data</BreadCrumb.Crumb>
+                <BreadCrumb.Crumb className="active">Data</BreadCrumb.Crumb>
+            </BreadCrumb>
+        );
+    }
+}; 
